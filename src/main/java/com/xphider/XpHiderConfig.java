faf53afc681279060ae7b5bde12a40ca1deedf63
david <dcarte51@msudenver.edu>
2024-11-06 16:15:52 -0700

    rename files and properties

--- src/main/java/com/xphider/XpHiderConfig.java
+++ src/main/java/com/xphider/XpHiderConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.xphider;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface XpHiderConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/test/java/com/xphider/XpHiderPluginTest.java
+++ src/test/java/com/xphider/XpHiderPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.xphider;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class XpHiderPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(XpHiderPlugin.class);
 		RuneLite.main(args);
 	}
 }
